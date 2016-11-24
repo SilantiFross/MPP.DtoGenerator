0c83437d6f5e7a3a9d3e358e5d3b0f14a22c8799
Silanti <silantifross@live.com>
2016-11-25 01:18:51 +0300

    Added code generation for one class

--- DtoParcer/DtoParcer/Generator.cs
+++ DtoParcer/DtoParcer/Generator.cs
@@ -2,10 +2,12 @@
 using System.IO;
 using System.Text;
 using DtoParcer.GenerationUnits;
+using DtoParcer.GenerationUnits.Components;
 using DtoParcer.Parcer.Table;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Formatting;
 using Newtonsoft.Json;
 
@@ -15,8 +17,8 @@ namespace DtoParcer
     {
         private readonly RouterGeneration _routerGeneration;
         private readonly ConfigApp _configApp;
+        private readonly TableTypes _tableTypes;
         private CollectionOfClasses _collectionOfClasses;
-        private TableTypes _tableTypes;
 
         public Generator(string pathToJson, string pathToGeneratedClasses, int numberOfMaxTasks, string namespaceClasses)
         {
@@ -29,27 +31,59 @@ namespace DtoParcer
         {
             _collectionOfClasses =
                 JsonConvert.DeserializeObject<CollectionOfClasses>(File.ReadAllText(_routerGeneration.PathToJson));
+
+            ParceCsFile(_collectionOfClasses.ClassDescriptions[0]);
         }
 
-        private void ParceCsFiles()
+        private void ParceCsFile(Class classDescription)
         {
             var namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName(_configApp.NamespaceClasses));
 
-            var classDeclaration = SyntaxFactory.ClassDeclaration(_collectionOfClasses.ClassDescriptions[0].ClassName)
+            var classDeclaration = SyntaxFactory.ClassDeclaration(classDescription.ClassName)
                 .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
                 .AddModifiers(SyntaxFactory.Token(SyntaxKind.SealedKeyword));
 
+            foreach (var property in classDescription.Properties)
+            {
+                var jsonType = new JsonType(property.Type, property.Format);
+                var propertyDeclaration =
+                    SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName(_tableTypes.GetNetType(jsonType)), property.Name)
+                        .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                        .WithModifiers(
+                    SyntaxFactory.TokenList(
+                        SyntaxFactory.Token(SyntaxKind.PublicKeyword)))
+                .WithAccessorList(
+                    SyntaxFactory.AccessorList(
+                        SyntaxFactory.List(
+                            new []{
+                                SyntaxFactory.AccessorDeclaration(
+                                    SyntaxKind.GetAccessorDeclaration)
+                                .WithKeyword(
+                                    SyntaxFactory.Token(SyntaxKind.GetKeyword))
+                                .WithSemicolonToken(
+                                    SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                                SyntaxFactory.AccessorDeclaration(
+                                    SyntaxKind.SetAccessorDeclaration)
+                                .WithKeyword(
+                                    SyntaxFactory.Token(SyntaxKind.SetKeyword))
+                                .WithSemicolonToken(
+                                    SyntaxFactory.Token(SyntaxKind.SemicolonToken))}))
+                    .WithOpenBraceToken(
+                        SyntaxFactory.Token(SyntaxKind.OpenBraceToken))
+                    .WithCloseBraceToken(
+                        SyntaxFactory.Token(SyntaxKind.CloseBraceToken)));
+                classDeclaration = classDeclaration.AddMembers(propertyDeclaration);
+            }
+
             namespaceDeclaration = namespaceDeclaration.AddMembers(classDeclaration);
-
             var compilationUnit = SyntaxFactory.CompilationUnit();
-
             compilationUnit = compilationUnit.AddMembers(namespaceDeclaration);
 
-            var cw = new AdhocWorkspace();
-            var options = cw.Options;
+            var workspace = new AdhocWorkspace();
+            var options = workspace.Options;
             options = options.WithChangedOption(CSharpFormattingOptions.SpaceBeforeOpenSquareBracket, false);
 
-            var formattedNode = Formatter.Format(compilationUnit, cw, options);
+            var formattedNode = Formatter.Format(compilationUnit, workspace, options);
             var stringBuilder = new StringBuilder();
 
             using (var writer = new StringWriter(stringBuilder))
