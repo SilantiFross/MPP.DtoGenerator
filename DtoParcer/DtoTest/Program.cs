e07a987b1e04c7492f925d10a72f19bdd0c307ea
Silanti <silantifross@live.com>
2016-11-25 22:37:14 +0300

    added output error reading from config

--- DtoParcer/DtoTest/Program.cs
+++ DtoParcer/DtoTest/Program.cs
@@ -10,14 +10,36 @@ namespace DtoTest
         {
             var pathToJson = args[0];
             var pathToCsFiles = args[1];
-            var numberOfMaxTasks = int.Parse(ConfigurationManager.AppSettings["numberOfTasks"]);
+            int numberOfMaxTasks;
             var namespaceClasses = ConfigurationManager.AppSettings["namespace"];
-            var generator = new Generator(pathToJson, pathToCsFiles, numberOfMaxTasks, namespaceClasses);
 
-            generator.GenerateClasses();
+            if (IsNumber(out numberOfMaxTasks))
+            {
+                var generator = new Generator(pathToJson, pathToCsFiles, numberOfMaxTasks, namespaceClasses);
+                generator.GenerateClasses();
+                WriteItsOkInConsole();
+            }
+            else
+            {
+                WriteConfigErrorInConsole();
+            }
+        }
 
+        private static void WriteItsOkInConsole()
+        {
             Console.WriteLine("It's ok");
             Console.ReadLine();
+        }
+
+        private static void WriteConfigErrorInConsole()
+        {
+            Console.WriteLine("Check app.config. key='numberOfTasks' is not a number");
+            Console.ReadLine();
+        }
+
+        private static bool IsNumber(out int numberOfMaxTasks)
+        {
+            return int.TryParse(ConfigurationManager.AppSettings["numberOfTasks"], out numberOfMaxTasks);
         }
     }
 }
