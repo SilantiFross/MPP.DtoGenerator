d25b098e9382d8e5e18b415b30c60113a8c322ca
Silanti <silantifross@live.com>
2016-11-28 22:54:56 +0300

    refactoring

--- DtoParcer/DtoParcer/AdministratorTasks.cs
+++ DtoParcer/DtoParcer/AdministratorTasks.cs
@@ -9,19 +9,19 @@ namespace DtoParcer
 {
     internal class AdministratorTasks
     {
-        private readonly Queue<Class> _tasksDictionary = new Queue<Class>();
+        private readonly Queue<Class> _taskQueue = new Queue<Class>();
         private readonly object _syncRoot = new object();
         private int _runningTasksCount;
         private readonly int _maxTasksCount;
 
-        private bool HasTasks => _tasksDictionary.Count > 0;
+        private bool HasTasks => _taskQueue.Count > 0;
         private bool CanAddToPool => _runningTasksCount < _maxTasksCount;
 
         public AdministratorTasks(int maxTasksCount)
         {
             _runningTasksCount = 0;
             _maxTasksCount = maxTasksCount;
-            _tasksDictionary.Clear();
+            _taskQueue.Clear();
         }
 
         public void GenerateCsClass(ConcurrentQueue<StringBuilder> generatedCsClasses, Class adddedClass, CountdownEvent countdownEvent, Func<Class, StringBuilder> parceFunc)
@@ -29,7 +29,7 @@ namespace DtoParcer
             if (CanAddToPool)
                 AddToPool(generatedCsClasses, adddedClass, countdownEvent, parceFunc);
             else
-                _tasksDictionary.Enqueue(adddedClass);
+                _taskQueue.Enqueue(adddedClass);
         }
 
         public void WaitAllTasks(CountdownEvent countdownEvent)
--- DtoParcer/DtoTest/Program.cs
+++ DtoParcer/DtoTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Text;
